72c7bf9b8081d307dae1932dc673a0df07468962
Arthur Masson <arthur.sw@gmail.com>
2024-12-12 16:09:16 +0100

    fix copy test data bugs

--- Scripts/copy_test_data.py
+++ Scripts/copy_test_data.py
@@ -8,11 +8,14 @@ parser.add_argument("-a", "--archive", help="Path to the transfer folder in whic
 parser.add_argument("-d", "--destination", help="Path to the destination source tools in which all test data will be copied. This arg must only be provided on the destination machine.", type=Path, default=None)
 args = parser.parse_args()
 
-source = args.archive is args.destination is not None and args.archive.exists() and args.archive.is_dir() else args.source
+source = args.archive if args.destination is not None and args.archive.exists() and args.archive.is_dir() else args.source
 destination = args.archive if args.destination is None else args.destination
 destination.mkdir(exist_ok=True, parents=True)
 
 for tool in sorted(list(args.source.iterdir())):
+    if not tool.is_dir(): continue
     tool_destination = destination / tool.name
     tool_destination.mkdir(exist_ok=True, parents=True)
-    shutil.copytree(tool / 'test-data', tool_destination / 'test-data')
+    test_data = tool / 'test-data'
+    if not test_data.exists(): continue
+    shutil.copytree(test_data, tool_destination / 'test-data')
